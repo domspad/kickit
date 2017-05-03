7e02e2aab0ff0120234a24c36ac2ca0fbe719c8a
Dominic <accounts.github@dominic.spadacene.com>
2017-05-03 15:41:05 -0400

    playing a simple loop

--- loop.py
+++ loop.py
@@ -1,10 +1,14 @@
-
+import asyncio
+import functools
 import glob
 import os
 import simpleaudio
 import time
 
 DATADIR = 'drumkit'
+
+BPM = 200 
+TIME_BETWEEN = 60 / BPM
 
 def base_path(filename):
     basename = os.path.basename(filename)
@@ -18,7 +22,21 @@ def play_sound(sound_name):
     sound = PLAY_SOUNDS[sound_name]
     sound.play()
 
-play_sound("kick")
-time.sleep(0.5)
-play_sound("kick")
-time.sleep(1)
+
+DRUM_PATTERN = {
+        'hhat': [0, 1, 0, 1, 0, 1, 0, 1],
+        'kick': [1, 0, 1, 0, 1, 1, 1, 1],
+        }
+BEATS_IN_PATTERN = 8
+
+def read_pattern_and_schedule(loop):
+    for sound, pattern in DRUM_PATTERN.items():
+        for idx, has_beat in enumerate(pattern):
+            if has_beat:
+                loop.call_later(TIME_BETWEEN * idx, functools.partial(play_sound, sound))
+    loop.call_later(TIME_BETWEEN * BEATS_IN_PATTERN, read_pattern_and_schedule, loop)
+
+loop = asyncio.get_event_loop()
+loop.call_soon(read_pattern_and_schedule, loop)
+loop.run_forever()
+
