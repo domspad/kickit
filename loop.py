9fd69dbbf8f37c45c007699086c4ac8169f12aa9
Elias Dorneles <eliasdorneles@gmail.com>
2017-05-04 14:30:22 -0400

    apply pep-8 style fixes

--- loop.py
+++ loop.py
@@ -9,15 +9,20 @@ DATADIR = 'drumkit'
 
 BPM = 120
 BEAT_RESOLUTION = 4
-TIME_BETWEEN = 60 / BPM / BEAT_RESOLUTION 
+TIME_BETWEEN = 60 / BPM / BEAT_RESOLUTION
+
 
 def base_path(filename):
     basename = os.path.basename(filename)
     name, ext = os.path.splitext(basename)
     return name
 
-PLAY_SOUNDS = { base_path(f) : simpleaudio.WaveObject.from_wave_file(f) 
-                    for f in glob.glob(DATADIR+'/*.wav') }
+
+PLAY_SOUNDS = {
+    base_path(f): simpleaudio.WaveObject.from_wave_file(f)
+    for f in glob.glob(DATADIR+'/*.wav')
+}
+
 
 def play_sound(sound_name):
     sound = PLAY_SOUNDS[sound_name]
@@ -44,7 +49,8 @@ DRUM_PATTERN = {
             0, 0, 0, 0,
             ],
         }
-BEATS_IN_PATTERN = len(DRUM_PATTERN['kick']) 
+BEATS_IN_PATTERN = len(DRUM_PATTERN['kick'])
+
 
 def read_pattern_and_schedule(loop):
     for sound, pattern in DRUM_PATTERN.items():
@@ -53,7 +59,8 @@ def read_pattern_and_schedule(loop):
                 loop.call_later(TIME_BETWEEN * idx, functools.partial(play_sound, sound))
     loop.call_later(TIME_BETWEEN * (BEATS_IN_PATTERN), read_pattern_and_schedule, loop)
 
-loop = asyncio.get_event_loop()
-loop.call_soon(read_pattern_and_schedule, loop)
-loop.run_forever()
 
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.call_soon(read_pattern_and_schedule, loop)
+    loop.run_forever()
